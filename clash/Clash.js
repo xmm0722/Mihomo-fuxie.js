d72020cd30c491c3ad00dc80f35f7245ee8e4336
Wans <wa.ns@qq.com>
2025-04-12 14:28:26 +0800

    refactor: 添加多订阅合并功能，优化配置管理

--- clash/Clash.js
+++ clash/Clash.js
@@ -1,13 +1,66 @@
 /**
  * 个人备份使用，该脚本适用与Mihomo Party和 Clash Verge Rev
- */
-
-/**
  * Clash Verge Rev 全局扩展脚本（懒人配置）/ Mihomo Party 覆写脚本
  * URL: https://github.com/wanswu/my-backup
  */
 
+// 多订阅合并，这里添加额外的地址
+const proxyProviders = {
+  "p1": {
+    "type": "http",
+    // 订阅 链接
+    "url": "https://baidu.com",
+    // 自动更新时间 21600(秒) / 3600 = 6小时
+    "interval": 21600,
+    // 节点文件 p1对应provider1
+    "path": "./proxy_provider/p1.yaml",
+    "override": {
+      // 节点名称前缀 p1，用于区别机场节点
+      "additional-prefix": "p1 |"
+    }
+  },
+  "p2": {
+    "type": "http",
+    "url": "https://google.com",
+    "interval": 21600,
+    "path": "./proxy_provider/p2.yaml",
+    "override": {
+      "additional-prefix": "p2 |"
+    }
+  },
+}
 
+// 程序入口
+function main(config) {
+  const proxyCount = config?.proxies?.length ?? 0;
+  const originalProviders = config?.["proxy-providers"] || {};
+  const proxyProviderCount = typeof originalProviders === "object" ? Object.keys(originalProviders).length : 0;
+
+  if (proxyCount === 0 && proxyProviderCount === 0) {
+    throw new Error("配置文件中未找到任何代理");
+  }
+
+  // 合并而非覆盖
+  config["proxy-providers"] = {
+    ...originalProviders,  // 保留原有配置
+    ...proxyProviders       // 合并新配置（同名则覆盖）
+  };
+  // 覆盖原配置中DNS配置
+  config["dns"] = dnsConfig;
+  // 覆盖原配置中的代理组
+  config["proxy-groups"] = proxyGroupConfig;
+  // 覆盖原配置中的规则
+  config["rule-providers"] = ruleProviders;
+  config["rules"] = rules;
+  //覆盖通用配置
+  config["mixed-port"] = "7890";
+  config["allow-lan"] = true;
+  config["bind-address"] = "*";
+  config["ipv6"] = true;
+  config["unified-delay"] = true;
+  // 返回修改后的配置
+  return config;
+}
 // DNS配置
 const dnsConfig = {
   "enable": true,
@@ -438,27 +491,3 @@ const rules = [
   "GEOIP,CN,DIRECT",
   "MATCH,Proxy"
 ];
-// 程序入口
-function main(config) {
-  const proxyCount = config?.proxies?.length ?? 0;
-  const proxyProviderCount =
-    typeof config?.["proxy-providers"] === "object" ? Object.keys(config["proxy-providers"]).length : 0;
-  if (proxyCount === 0 && proxyProviderCount === 0) {
-    throw new Error("配置文件中未找到任何代理");
-  }
-  // 覆盖原配置中DNS配置
-  config["dns"] = dnsConfig;
-  // 覆盖原配置中的代理组
-  config["proxy-groups"] = proxyGroupConfig;
-  // 覆盖原配置中的规则
-  config["rule-providers"] = ruleProviders;
-  config["rules"] = rules;
-  //覆盖通用配置
-  config["mixed-port"] = "7890";
-  config["allow-lan"] = true;
-  config["bind-address"] = "*";
-  config["ipv6"] = true;
-  config["unified-delay"] = true;
-  // 返回修改后的配置
-  return config;
-}
