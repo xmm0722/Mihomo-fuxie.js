2fb76824f8151fa4f2b17ac5f058e915728093f0
Wans <wa.ns@qq.com>
2025-04-15 13:56:01 +0800

    更新 Clash.js 中的 BiliBili 代理组名称为 BiliBili，并调整相关规则提供者的 URL 和路径，以支持新的配置。

--- clash/Clash.js
+++ clash/Clash.js
@@ -142,9 +142,9 @@ const proxyGroupConfig = [
   },
   {
     ...groupBaseOption,
-    "name": "BiliBiliTW",
+    "name": "BiliBili",
     "type": "select",
-    "proxies": ["Proxy", "DIRECT", "REJECT", "HK", "TW", "JP", "KR", "US", "DE", "SG", "FR", "UK"],
+    "proxies": ["DIRECT", "Proxy", "REJECT", "HK", "TW", "JP", "KR", "US", "DE", "SG", "FR", "UK"],
     "include-all": true,
     "exclude-filter": "(?i)GB|Traffic|Expire|Premium|频道|订阅|ISP|流量|到期|重置",
     "icon": "https://cdn.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/bilibili.png"
@@ -378,11 +378,11 @@ const ruleProviders = {
     "url": "https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/YouTube/YouTube.yaml",
     "path": "./ruleset/YouTube.yaml"
   },
-  "BiliBiliTW": {
+  "BiliBili": {
     ...ruleProviderCommon,
     "behavior": "classical",
-    "url": "https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/BiliBiliIntl/BiliBiliIntl.yaml",
-    "path": "./ruleset/BiliBiliTW.yaml"
+    "url": "https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/BiliBili/BiliBili.yaml",
+    "path": "./ruleset/BiliBili.yaml"
   },
   "TikTok": {
     ...ruleProviderCommon,
@@ -465,7 +465,7 @@ const ruleProviders = {
   "Github": {
     ...ruleProviderCommon,
     "behavior": "classical",
-    "url": "https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/GitHub/GitHub.yaml",
+    "url": "https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/GitHub/GitHub_No_Resolve.yaml",
     "path": "./ruleset/Github.yaml"
   },
 };
@@ -475,7 +475,7 @@ const rules = [
   // 自定义规则
   "RULE-SET,Telegram,Telegram",
   "RULE-SET,YouTube,YouTube",
-  "RULE-SET,BiliBiliTW,BiliBiliTW",
+  "RULE-SET,BiliBili,BiliBili",
   "RULE-SET,TikTok,TikTok",
   "RULE-SET,Spotify,Spotify",
   "RULE-SET,Netflix,Netflix",
